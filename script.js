eb204fa5f378c7e4f160b2cc3f2ce8c0def78921
ZeRobertoCF <zerobertocabezudo@gmail.com>
2024-09-11 13:09:46 -0500

    projecto 2

---
+++ script.js
@@ -0,0 +1,113 @@
+function mostrartexto() {
+    var textoDiv = document.getElementById('texto');
+    if (textoDiv.style.display === 'none' || textoDiv.style.display === '') {
+        textoDiv.style.display = 'flex';
+    } else {
+        textoDiv.style.display = 'none';
+    }
+}
+
+function mostrartexto1() {
+    var textoDiv = document.getElementById('texto1');
+    if (textoDiv.style.display === 'none' || textoDiv.style.display === '') {
+        textoDiv.style.display = 'flex';
+    } else {
+        textoDiv.style.display = 'none';
+    }
+}
+function mostrartexto2() {
+    var textoDiv = document.getElementById('texto2');
+    if (textoDiv.style.display === 'none' || textoDiv.style.display === '') {
+        textoDiv.style.display = 'flex';
+    } else {
+        textoDiv.style.display = 'none';
+    }
+}
+function mostrartexto3() {
+    var textoDiv = document.getElementById('texto3');
+    if (textoDiv.style.display === 'none' || textoDiv.style.display === '') {
+        textoDiv.style.display = 'flex';
+    } else {
+        textoDiv.style.display = 'none';
+    }
+}
+function mostrartexto4() {
+    var textoDiv = document.getElementById('texto4');
+    if (textoDiv.style.display === 'none' || textoDiv.style.display === '') {
+        textoDiv.style.display = 'flex';
+    } else {
+        textoDiv.style.display = 'none';
+    }
+}
+function mostrartexto5() {
+    var textoDiv = document.getElementById('texto5');
+    if (textoDiv.style.display === 'none' || textoDiv.style.display === '') {
+        textoDiv.style.display = 'flex';
+    } else {
+        textoDiv.style.display = 'none';
+    }
+}
+function mostrartexto6() {
+    var textoDiv = document.getElementById('texto6');
+    if (textoDiv.style.display === 'none' || textoDiv.style.display === '') {
+        textoDiv.style.display = 'flex';
+    } else {
+        textoDiv.style.display = 'none';
+    }
+}
+
+function mostrartexto10() {
+    var textoDiv = document.getElementById('textos1');
+    if (textoDiv.style.display === 'none' || textoDiv.style.display === '') {
+        textoDiv.style.display = 'flex';
+    } else {
+        textoDiv.style.display = 'none';
+    }
+}
+function mostrartexto11() {
+    var textoDiv = document.getElementById('textos2');
+    if (textoDiv.style.display === 'none' || textoDiv.style.display === '') {
+        textoDiv.style.display = 'flex';
+    } else {
+        textoDiv.style.display = 'none';
+    }
+}
+
+function mostrartexto12() {
+    var textoDiv = document.getElementById('textos3');
+    if (textoDiv.style.display === 'none' || textoDiv.style.display === '') {
+        textoDiv.style.display = 'flex';
+    } else {
+        textoDiv.style.display = 'none';
+    }
+}
+function mostrartexto14() {
+    var textoDiv = document.getElementById('textos4');
+    if (textoDiv.style.display === 'none' || textoDiv.style.display === '') {
+        textoDiv.style.display = 'flex';
+    } else {
+        textoDiv.style.display = 'none';
+    }
+}
+function mostrartexto15() {
+    var textoDiv = document.getElementById('textos5');
+    if (textoDiv.style.display === 'none' || textoDiv.style.display === '') {
+        textoDiv.style.display = 'flex';
+    } else {
+        textoDiv.style.display = 'none';
+    }
+}
+function mostrartexto16() {
+    var textoDiv = document.getElementById('textos6');
+    if (textoDiv.style.display === 'none' || textoDiv.style.display === '') {
+        textoDiv.style.display = 'flex';
+    } else {
+        textoDiv.style.display = 'none';
+    }
+}
+
+const scrollToTopBtn = document.getElementById('scrollToTopBtn');
+
+scrollToTopBtn.onclick = function () {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+};
